9797f7c87249d97f345e56facc80dd76be87021e
vidhyuthdev <144331752+vidhyuthdev@users.noreply.github.com>
2025-02-11 10:17:27 +0530

    Landing Page Responsive fixes

--- frontend/car-rental/src/Pages/LandingPage/AboutUs.jsx
+++ frontend/car-rental/src/Pages/LandingPage/AboutUs.jsx
@@ -3,7 +3,7 @@ const AboutUs = () => {
     <div className='flex flex-col items-center mt-4 mx-auto'>
       <div className='text-4xl text-custom-dark font-extrabold'>About Us</div>
       <div className='mx-5 grid grid-rows-3 sm:grid-cols-3 sm:grid-rows-1 mt-5'>
-        <div className='flex flex-col items-center justify-center sm:border-r sm:border-r-gray-500'>
+        <div className='flex flex-col items-center justify-center sm:border-r sm:border-r-gray-500 pr-3'>
             <div className='text-custom-dark text-4xl font-bold text-center'>50,000+</div>
             <div className='text-custom-dark text-xl text-center'>Thousands of happy customers drive with us every day."</div>
 
--- frontend/car-rental/src/Pages/LandingPage/HowItWorks.jsx
+++ frontend/car-rental/src/Pages/LandingPage/HowItWorks.jsx
@@ -1,31 +1,6 @@
 import Cards from "./Cards"
-
-const HowItWorks = () => {
-
-    const cardData=[
-        {
-            id:'01',
-            heading:'Find a car',
-            desc:'Browse through our fleet and choose the perfect car that fits your needs and budget.Filter by model, price, or features to make your selection effortless.'
-        },
-        {
-            id:'02',
-            heading:'Book rental',
-            desc:'Select your rental dates and confirm availability with just a few clicks. Flexible booking options let you plan your trip hassle-free.'
-        },
-        {
-            id:'03',
-            heading:'Complete Payment',
-            desc:'Secure your booking with an easy and transparent payment process. We accept multiple payment methods with no hidden charges.',
-        },
-        {
-            id:'04',
-            heading:'Booking Confirmed!',
-            desc:'Get instant confirmation along with pickup details and car information. Simply arrive, collect your car, and hit the road with confidence!'
-
-        },
-
-    ]
+import cardData from "../../assets/Data/LandingPageCard";
+const HowItWorks = () => {    
   return (
     <div className="bg-custom-dark pb-10">
         <div className=' mt-6 text-center text-custom-lightblue font-bold text-4xl'>
@@ -39,12 +14,11 @@ const HowItWorks = () => {
                         id={data.id} 
                         heading={data.heading} 
                         desc={data.desc} 
-                        key={data.id} 
+                        key={data.keyVal} 
                         />
                     </div>
                 ))
             }
-
         </div>
     </div> )
 }
