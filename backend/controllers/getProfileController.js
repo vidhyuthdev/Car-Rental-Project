8356392243f4c45a6a0bc48075c0fa5653ab1b1b
vidhyuthdev <144331752+vidhyuthdev@users.noreply.github.com>
2025-04-16 19:21:57 +0530

    Commit

--- backend/controllers/getProfileController.js
+++ backend/controllers/getProfileController.js
@@ -1,25 +1,3 @@
-// const { User,Bookings } = require('../Models/User');
-
-// const getProfileController = async (req, res, next) => {
-//   const { email } = req.body;
-
-//   try {
-//     const user = await User.findByPk(email, {
-//       attributes: ['email', 'name', 'mobile'] 
-//     });
-
-//     if (!user) {
-//       return res.status(404).json({ msg: 'User not found' });
-//     }
-
-//     return res.status(200).json(user);
-//   } catch (error) {
-//     console.error("Error fetching user profile:", error);
-//     return res.status(500).json({ msg: 'Internal Server Error' });
-//   }
-// };
-
-// module.exports = getProfileController;
 const { User, Bookings } = require('../Models/User');
 
 const getProfileController = async (req, res, next) => {
